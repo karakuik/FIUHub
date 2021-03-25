2e7dbffb1a625e4664b4095355dd56456026e580
karakuik <wesloke@gmail.com>
2021-03-25 19:03:56 -0400

    3/22 - RM

--- FIUHub/src/fiuhub/FIUHub.java
+++ FIUHub/src/fiuhub/FIUHub.java
@@ -19,6 +19,7 @@ public class FIUHub {
         
         System.out.println("Hello World! Commit #1");
         System.out.println("This is FIUHub, a chat program.");
+        System.out.println("Testesttest :)");
         
         //code to test Credentials.java
         Credentials cred = new Credentials();
